31973fd8ce540ba7fcfa3c79f19d4e49413c1f71
Jason Crowe (Mozilla) <jcrowe@mozilla.com>
2017-04-10 10:48:09 -0400

    Updates for code review

--- bin/dependencies.sh
+++ bin/dependencies.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
-# shellcheck disable=SC2034
-# https://github.com/koalaman/shellcheck/wiki/SC2034
 #
 # This is a collection of functions to test for dependancies
 #
 
 test_for_packer () {
-    TEST=$(hash packer 2>&1) 2> /dev/null
-    if [ $? != 0 ]; then
+    if ! hash packer > /dev/null 2>&1; then
         message_print 'CRITICAL' 'packer must be installed and on your path!'
         message_print 'CRITICAL' 'See: https://packer.io/'
         message_print 'VERBOSE' "File: '${BASH_SOURCE[0]}' Line: '${LINENO}'"
@@ -16,8 +13,7 @@ test_for_packer () {
 }
 
 test_for_jq () {
-    TEST=$(hash jq 2>&1) 2> /dev/null
-    if [ $? != 0 ]; then
+    if ! hash jq > /dev/null 2>&1; then
         message_print 'CRITICAL' 'jq must be installed and on your path!'
         message_print 'CRITICAL' 'See: https://github.com/stedolan/jq'
         message_print 'VERBOSE' "File: '${BASH_SOURCE[0]}' Line: '${LINENO}'"
@@ -26,8 +22,7 @@ test_for_jq () {
 }
 
 test_for_parallel () {
-    TEST=$(hash parallel 2>&1) 2> /dev/null
-    if [ $? != 0 ]; then
+    if ! hash parallel > /dev/null 2>&1; then
         message_print 'CRITICAL' 'parallel must be installed and on your path!'
         message_print 'VERBOSE' "File: '${BASH_SOURCE[0]}' Line: '${LINENO}'"
         exit_clean 1
@@ -35,8 +30,7 @@ test_for_parallel () {
 }
 
 test_for_aws_vault () {
-    TEST=$(hash aws-vault 2>&1) 2> /dev/null
-    if [ $? != 0 ]; then
+    if ! hash aws-vault > /dev/null 2>&1; then
         message_print 'CRITICAL' 'aws-vault must be installed and on your path!'
         message_print 'CRITICAL' 'See: https://github.com/99designs/aws-vault'
         message_print 'VERBOSE' "File: '${BASH_SOURCE[0]}' Line: '${LINENO}'"
